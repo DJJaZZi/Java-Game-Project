50681a393d9903739e4d9d7557b3636ead049cdb
DJJaZZi <siganak18@gmail.com>
2026-05-06 10:14:29 +0500

    LevelCompleteState added

---
+++ core/src/main/java/com/roguelike/core/game/LevelCompleteState.java
@@ -0,0 +1,28 @@
+package com.roguelike.core.game;
+
+import com.roguelike.input.PlayerAction;
+
+public class LevelCompleteState implements GameState {
+
+    @Override
+    public void onEnter(GameManager gm) {
+        System.out.println("[LevelCompleteState] Level complete! Score: " + gm.getScore());
+    }
+
+    @Override
+    public void update(GameManager gm, float deltaTime) {}
+
+    @Override
+    public void handleInput(GameManager gm, PlayerAction action) {
+        // Any key advances to the next level
+        gm.nextLevel();
+    }
+
+    @Override
+    public void onExit(GameManager gm) {}
+
+    @Override
+    public GameStateType getType() {
+        return GameStateType.LEVEL_COMPLETE;
+    }
+}
